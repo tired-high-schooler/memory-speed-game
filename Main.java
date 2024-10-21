ecd89e44db6e34ba0c604f3ea425202e2404bb91
Case X B [STUDENT] <browncx@limestone.on.ca>
2024-10-21 13:06:11 -0400

    worked on main loop sources: https://stackoverflow.com/questions/5328996/java-change-int-to-ascii, https://www.callicoder.com/java-arraylist/#:~:text=ArrayList%20is%20part%20of%20Java's%20collection%20framework%20and%20implements%20Java's%20List%20interface.&text=An%20ArrayList%20is%20a%20re,array%20to%20store%20the%20elements., https://stackoverflow.com/questions/24104313/how-do-i-make-a-delay-in-java, https://stackoverflow.com/questions/7811468/unresolved-compilation-unhandled-exception-type-ioexception

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import time;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 class main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -8,21 +11,34 @@ class main{
         int rand1;
         int rand2;
         int rand3;
-        int modNum;
+        int modNum = 0;
+        int points = 0;
+        String responce;
+        boolean running;
+
+        ArrayList<Character> achar = new ArrayList<Character>();
         if(mode >= 1 && mode <= 3){
-            boolean running = true;
+            running = true;
         }
         else{
             running = false;
         }
 
-        while(running){
-            rand1 = Math.random() *modNum;
-            rand2 = Math.random() *range;
-            rand3 =Math .random() *range;
+        while(running && points < 1000 && points > -100){
+            rand1 = (int)Math.random() *modNum;
+            rand2 = (int)Math.random() *range;
+            rand3 = (int)Math.random() *range;
 
             switch(mode){
                 case 1:
+                    achar.add((char) (rand1 + 97));
+                    for(int i = 0; i < achar.size()  ; i++){
+                        System.out.print(achar.get(i));
+                    }
+                    TimeUnit.SECONDS.sleep(1);
+                    System.out.print("\033[2J\033[H");
+                    System.out.println("reapeat: ");
+                    responce = sc.nextLine();
                     break;
                 case 2:
                     break;
