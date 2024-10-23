a97a4e15c60577cdc1a98c7808f27a544768e0ec
some dude <browncx@limestone.on.ca>
2024-10-23 13:08:18 -0400

    Finished first mode, completed ending, completed other 2 modes, and did some tidying up. No sources were used

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import time;
 import java.util.ArrayList;
-class main{
+class Main{
     public static void main(String[] args) throws InterruptedException{
         Scanner sc = new Scanner(System.in);
         System.out.print("\nEnter mode: 1, 2, or 3: ");
@@ -13,9 +13,14 @@ class main{
         int rand3;
         int modNum = 0;
         int points = 0;
-        String responce;
+        String responceC;
+        int responceI;
         boolean running;
         boolean correct;
+        boolean mod = false;
+        String guessingUnits;
+        int roundPoints = 0;
+        int sum = 0;
 
 
         ArrayList<Character> achar = new ArrayList<Character>();
@@ -25,48 +30,152 @@ class main{
         else{
             running = false;
         }
+        switch(mode){
+            case 1:
+                range = 26;
+                guessingUnits = "character";
+                break;
+            case 2:
+                range = 10;
+                guessingUnits = "digit";
+                break;
+            case 3:
+                range = 100;
+                guessingUnits = "digit";
+                System.out.print(0);
+                break;
+            default:
+                guessingUnits = "error";
+                System.exit(0);
+                break;
+        }
+    
 
-        while(running && points < 1000 && points > -100){
+        while(running && points < 100 && points > -100){
+
+            roundPoints = 0;
             correct = false;
-            rand1 = (int)Math.random() *modNum;
-            rand2 = (int)Math.random() *range;
+            if(points >= 50){
+                rand1 = (int)Math.random() *modNum;
+            }
+            else{
+                rand1 = 0;
+            }
+            rand2 = (int)(Math.random() *range);
             rand3 = (int)Math.random() *range;
 
             switch(mode){
                 case 1:
                     correct = true;
-                    achar.add((char) (rand1 + 97));
+                    achar.add((char) (rand2 + 97));
                     for(int i = 0; i < achar.size()  ; i++){
                         System.out.print(achar.get(i));
                     }
                     Thread.sleep(1000);
                     System.out.print("\033[2J\033[H");
-                    System.out.println("reapeat: ");
-                    responce = sc.nextLine();
+                    System.out.println("repeat: ");
+                    responceC = sc.nextLine();
                    
-                    for(int i = 0; i < achar.size() && responce.length() < i  ; i++){
-                        System.out.print(achar.get(i));
-                        if(achar.get(i) != responce.charAt(i)){
-                            correct = false;
-                        }
-                    }
-                    System.out.print("\033[2J\033[H");
-                    if(correct){
-                        System.out.print("correct");
+                    if (responceC.length() != achar.size()){
+                        correct = false;
                     }
                     else{
-                        System.out.print("you are an idiot");
+                        for(int i = 0; i + 1 <= achar.size(); i++){
+                            if(achar.get(i) != responceC.charAt(i)){
+                                correct = false;
+                            }
+                        }
                     }
+                    if(correct){
+                        roundPoints += 10;
+                    }
+                    else{
+                        roundPoints -= 5;
+                    }
+
                     Thread.sleep(1000);
                     System.out.print("\033[2J\033[H");
 
 
                     break;
                 case 2:
+                    correct = true;
+                    achar.add((char) (rand2 + 48));
+                    for(int i = 0; i < achar.size()  ; i++){
+                        System.out.print(achar.get(i));
+                    }
+                    Thread.sleep(1000);
+                    System.out.print("\033[2J\033[H");
+                    System.out.println("repeat: ");
+                    responceC = sc.nextLine();
+                
+                    if (responceC.length() != achar.size()){
+                        correct = false;
+                    }
+                    else{
+                        for(int i = 0; i + 1 <= achar.size(); i++){
+                            if(achar.get(i) != responceC.charAt(i)){
+                                correct = false;
+                            }
+                        }
+                    }
+                    if(correct){
+                        roundPoints += 5;
+                    }
+                    else{
+                        roundPoints -= 5;
+                    }
+
+                    Thread.sleep(1000);
+                    System.out.print("\033[2J\033[H");
                     break;
                 case 3:
+                    System.out.println(" + " + rand2);
+                    sum += rand2;
+                    Thread.sleep(1000);
+                    System.out.print("\033[2J\033[H");
+                    System.out.println("answer: ");
+                    responceI = sc.nextInt();
+                    if(responceI == sum){
+                        correct = true;
+                    }
+                    if(correct){
+                        roundPoints += 15;
+                    }
+                    else{
+                        roundPoints -= 5;
+                    }
+                    System.out.print("\033[2J\033[H");
+
+                    
+
                     break;
+                
             }
+            points += roundPoints;
+            if(correct){
+                System.out.println("Correct!");
+            }
+            else{
+                System.out.println("failed");
+            }
+            System.out.println("you got: " + roundPoints);
+            System.out.println("total points: " + points);
+            Thread.sleep(1000);
+            System.out.print("\033[2J\033[H");
+
+            Thread.sleep(1000);
+        }
+        if(points >= 100){
+            System.out.print("\033[2J\033[H");
+            System.out.println("Victory");
+            if(mode != 3){
+                System.out.println("you just remembered a " + achar.size() + " " + guessingUnits + " sequnce!");
+            }
+            
+        }
+        else{
+            System.out.println("Game Over.");
         }
     }
 }
